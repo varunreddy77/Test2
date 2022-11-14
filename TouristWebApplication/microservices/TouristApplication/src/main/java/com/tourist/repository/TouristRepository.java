db355ef5ba4f363c4a3aaa76d9b98ad6ff91bfb4
varunreddy77 <82889593+varunreddy77@users.noreply.github.com>
2022-11-14 21:00:33 +0530

    Delete TouristWebApplication directory

--- TouristWebApplication/microservices/TouristApplication/src/main/java/com/tourist/repository/TouristRepository.java
+++
@@ -1,9 +0,0 @@
-package com.tourist.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.tourist.entity.Tourist;
-
-public interface  TouristRepository extends CrudRepository<Tourist,Integer> {
-
-};
